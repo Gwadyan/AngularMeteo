459823be178a8ad162a0c8b4df02e05bd2d2d71b
marc urbino <marc.urbino@neos-sdi.com>
2024-03-19 10:07:05 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { AddCardComponent } from './add-card/add-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HomeComponent],
+  imports: [CommonModule, RouterOutlet,HomeComponent,WeatherCardComponent,AddCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
