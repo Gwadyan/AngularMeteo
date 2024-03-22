4ed606b0eef43a27c81dba023d89bd9c2a09a3d4
marc urbino <marc.urbino@neos-sdi.com>
2024-03-22 14:46:31 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { AddCardComponent } from './add-card/add-card.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HomeComponent,WeatherCardComponent,AddCardComponent],
+  imports: [CommonModule, RouterOutlet,RouterLink,RouterLinkActive,HomeComponent,WeatherCardComponent,AddCardComponent,DetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'LabScss';
+  title = 'Météo';
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { WeatherCardComponent } from '../weather-card/weather-card.component';
+import { AddCardComponent } from '../add-card/add-card.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [WeatherCardComponent,AddCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
